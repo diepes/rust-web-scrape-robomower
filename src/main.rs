daeaf961ca4f6a5c79d279ea37e1813b542c1b95
Pieter E Smit <diepes@github.com>
2023-05-13 17:23:36 +1200

    cargo add anyow for errors async query implemented

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
-use tokio;
+//use std::array;
+
+use log::debug;
+//use log::error;
+use log::info;
+use anyhow::{Result};  //generic errors
 use reqwest;
 use serde::Deserialize;
-use log::debug;
-use log::error;
-use log::info;
+use tokio;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    env_logger::init();
-    info!("Start!");
-    for i in 1..=99 {
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
+    query_counties(1, 25).await?;
+    Ok(())
+}
+async fn query_counties(start: usize, end: usize) -> Result<(), Box<dyn std::error::Error>> {
+    info!("query_counties {start}..{end} Info:202305 countryId's 1, 6, 7, 8, 9, 10, 14, 17, 18, 19, 24");
+    let mut handles = Vec::new();
+    for i in start..=end {
         debug!("i = {i}");
         let url = format!(
             "{url_base}/{uri}?{query}",
@@ -18,12 +26,18 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             uri = "WebData/GetMPageImgs",
             query = format!("countryId={i}"),
         );
-        let count = get(&url).await?;
-        info!("Check count={count} url={url}");
+        handles.push((i, url.clone(), tokio::spawn(get(url))));
+    }
+    for (i, url, handle) in handles {
+        let count = handle.await.unwrap()?;
+        if count > 0 {
+            info!("🔥 i={i} count={count} url={url}");
+        } else {
+            debug!("* i={i} count={count} url={url}");
+        };
     }
     Ok(())
 }
-
 
 #[derive(Deserialize, Debug)]
 #[serde(deny_unknown_fields, rename_all = "PascalCase")]
@@ -34,7 +48,7 @@ struct Products {
     style_two_url: String,
 }
 
-async fn get(url: &str) -> Result<usize, Box<dyn std::error::Error>> {
+async fn get(url: String) -> Result<usize> {
     let client = reqwest::Client::new();
     let request = client
         .get(url)
@@ -42,7 +56,7 @@ async fn get(url: &str) -> Result<usize, Box<dyn std::error::Error>> {
         //.header(reqwest::header::CONTENT_TYPE, "application/json")
         .header(reqwest::header::ACCEPT, "application/json")
         .header(reqwest::header::REFERER, "https://www.yardforce-tools.com/");
-    debug!("Debug request={:?}",request);
+    debug!("Debug request={:?}", request);
     let response = request.send().await?;
     let text = response.text().await?;
     debug!("response = {:?}  len={}", text, text.len());
